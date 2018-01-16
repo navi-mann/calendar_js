ef81f74d2c6271c8100d6a5a2d7c037fe32dea29
Navi Mann <navdeep.k.mann@gmail.com>
2018-01-16 17:04:34 -0500

    created calendar using plain html & css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const $ = require('jquery');
 
 
-$(document).ready(function(){
-  $('body').html('jquery is working lolo')
-});
+// $(document).ready(function(){
+//   $('body').html('jquery is working lolo')
+// });
