ef5c89e8c10e5f57653f9f1e187cdcbfdbd006a1
Navi Mann <navdeep.k.mann@gmail.com>
2018-01-16 21:12:46 -0500

    added caption for current month

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const $ = require('jquery');
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 const date = new Date();
 date.setDate(1);
 const firstDay = date.getDay();
 console.log($('td')[firstDay]);
 const dayOfWeek = $('td')[firstDay].innerHTML = 1;
+const currentMonth = date.getMonth();
+console.log(currentMonth);
+$('.month').html(monthNames[currentMonth]);
 
 function daysInMonth(anyDateInMonth) {
     return new Date(anyDateInMonth.getYear(),
