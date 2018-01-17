02041dc7b41e6ad38e452c4875c650943f89eac6
Navi Mann <navdeep.k.mann@gmail.com>
2018-01-16 20:39:51 -0500

    now rendering calendar for current month

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const $ = require('jquery');
+const date = new Date();
+date.setDate(1);
+const firstDay = date.getDay();
+console.log($('td')[firstDay]);
+const dayOfWeek = $('td')[firstDay].innerHTML = 1;
 
+function daysInMonth(anyDateInMonth) {
+    return new Date(anyDateInMonth.getYear(),
+                    anyDateInMonth.getMonth()+1,
+                    0).getDate();}
 
-// $(document).ready(function(){
-//   $('body').html('jquery is working lolo')
-// });
+console.log(daysInMonth(new Date));
+for (var i = firstDay; i < daysInMonth(new Date) + 1; i ++) {
+  $('td')[i].innerHTML = i;
+}
